78d83c48742d92cb9cee7b1805995f74eb79d3bd
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-05-03 10:56:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let number1 = 0
+input.onButtonPressed(Button.A, function () {
+    number1 = 0
+})
+function guess (num: number) {
+    if (num == 0) {
+    	
+    }
+}
+basic.forever(function () {
+	
+})
